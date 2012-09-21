1bf1040a7386859e07165600d0cef4f2099bb85b
Michael Schaefermeyer <michael.schaefermeyer@zweitag.de>
2012-09-21 10:12:35 +0200

    Refactor fragment to call super only once
    As suggested by @kirel

--- lib/cache_comment/fragment_helper.rb
+++ lib/cache_comment/fragment_helper.rb
@@ -10,7 +10,8 @@ module CacheComment
     def read_fragment(key, options)
       unless params[:cache_comments]
         comment = CommentFormatter.new(fragment_cache_key(key), options)
-        super(key, options) && super(key, options).gsub(comment.start_regex, '').gsub(comment.end_regex, '')
+        fragment = super(key, options)
+        fragment.gsub(comment.start_regex, '').gsub(comment.end_regex, '') unless fragment.nil?
       else
         super key, options
       end
